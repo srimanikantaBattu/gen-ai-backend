0da6c226da7028e309a645024f1519d04c36c8eb
srimanikantaBattu <srimanikantabattu04@gmail.com>
2024-10-02 20:24:07 +0530

    Added vercel.json

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const { MongoClient } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config();
 
-app.use(cors());
+app.use(cors(
+  {
+  origin: ["http://localhost:3000"],
+  methods: ["GET", "POST" , "PUT", "DELETE"],
+  credentials: true
+  }
+));
 app.use(exp.json()); // Ensure JSON middleware is used
 
 const DB_URL = process.env.DB_URL;
